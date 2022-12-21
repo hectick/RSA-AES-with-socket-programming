d6858234bfe50ce97e507ff9aae29b9c5144522a
hectick <chyeon1223@cau.ac.kr>
2022-12-21 22:21:36 +0900

    get feedback and modified

--- client_file/client.go
+++ client_file/client.go
@@ -17,97 +17,121 @@ import (
 	"syscall"
 )
 
+var receiveChannel = make(chan []byte)
+var inputChannel = make(chan string)
+var sendChannel = make(chan []byte)
+
 func main() {
 	/* 서버 연결 정보 */
 	serverName := "127.0.0.1"
 	serverPort := "12345"
-	conn, _ := net.Dial("tcp", serverName+":"+serverPort)
+	conn, _ := net.Dial("tcp", serverName+":"+serverPort) //tcp 프로토콜 사용. 서버의 IP와 포트번호를 설정해줌. 설정된 목적지로 데이터를 전송함.
 	localAddr := conn.LocalAddr().(*net.TCPAddr)
 	fmt.Printf("## Client port number : %d ##\n\n", localAddr.Port)
 
 	/* 서버로부터 공개키를 받는 부분 */
-	buffer := make([]byte, 2048)
-	conn.Read(buffer)
-	sDec, _ := base64.StdEncoding.DecodeString(string(buffer))
+	buffer := make([]byte, 2048)                               //버퍼를 만들고 서버로부터 메세지가 오기를 기다림
+	conn.Read(buffer)                                          //버퍼안에 내용물이 들어오면 메세지를 읽음
+	sDec, _ := base64.StdEncoding.DecodeString(string(buffer)) //디코딩해서 공개키를 저장
 	publicKey := &rsa.PublicKey{}
-	json.Unmarshal(sDec, publicKey)
+	json.Unmarshal(sDec, publicKey) //공개키 형식 다듬기
 	fmt.Printf("## Receive public key from Server ##\n")
-	fmt.Printf("[Public key] %x\n\n", publicKey)
+	fmt.Printf("[Public key] %x\n\n", publicKey) //공개키 프린트
 
 	/* 대칭키 생성 */
 	fmt.Printf("Write 16byte for creating symmetric key >> ")
-	symmetricKey, _ := bufio.NewReader(os.Stdin).ReadString('\n') 
+	symmetricKey, _ := bufio.NewReader(os.Stdin).ReadString('\n') //대칭키를 입력해줌
 	symmetricKey = strings.TrimSpace(symmetricKey)
-	for len(symmetricKey) != 16 { 
+	for len(symmetricKey) != 16 { //대칭키가 16바이트가 아니면 다시 입력하도록 함
 		fmt.Printf("Retry : Write 16byte for creating symmetric key >> ")
 		symmetricKey, _ = bufio.NewReader(os.Stdin).ReadString('\n')
 		symmetricKey = strings.TrimSpace(symmetricKey)
 	}
 
-	/* 대칭키를 공개키로 암호화 */
+	/* 대칭키를 공개키로 암호화 - 평문을 공개키 암호화 코드를 구글링으로 찾음*/
 	ciphertext, err := rsa.EncryptPKCS1v15(
 		rand.Reader,
-		publicKey,            
-		[]byte(symmetricKey), 
+		publicKey,            //공개키를 이용해서
+		[]byte(symmetricKey), //대칭키를 암호화
 	)
 	if err != nil {
 		os.Exit(0)
 	}
 
 	/* 대칭키 전송 */
-	sendMsg := base64.StdEncoding.EncodeToString(ciphertext) 
+	sendMsg := base64.StdEncoding.EncodeToString(ciphertext) //인코딩
 	fmt.Printf("\n## Send symmetric key to Server \n")
 	fmt.Printf("[Symmetric key] %s\n", symmetricKey)
 	fmt.Printf("[Ciphertext] %x\n\n", ciphertext)
-	conn.Write([]byte(sendMsg)) 
+	conn.Write([]byte(sendMsg)) //서버에게 대칭키 전송
 
 	/* AES 대칭키 암호화 블록 생성 */
-	block, _ := NewAesCipher([]byte(symmetricKey)) 
+	block, _ := NewAesCipher([]byte(symmetricKey)) //대칭키 암호화에 사용할 블록을 생성해줌
 
-	go func() {
+	/* ctrl+c를 눌러서 종료할때 서버에게 연결 닫으라고 알려주는 부분 */
+	go func() { //go routine 사용. 프로그램을 실행하다가 언제든 ctrl+c를 누르면 서버에게 종료 메세지가 송신되도록 동작.
 		signals := make(chan os.Signal, 1)
 		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 		<-signals
 		fmt.Println("## Disconnected ##")
-		ciphertext := block.Encrypt("0")
+		ciphertext := block.Encrypt("0") //0은 종료를 의미
 		sendMsg := base64.StdEncoding.EncodeToString(ciphertext)
 		conn.Write([]byte(sendMsg))
 		conn.Close()
 		os.Exit(0)
 	}()
 
-	/* 서버에서 메세지를 받는 부분 */
-	go func() { 
-		for {
-			buffer := make([]byte, 2048)
-			_, err := conn.Read(buffer) 
-			if err != nil {
-				continue
-			}
-			ciphertextFromServer, _ := base64.StdEncoding.DecodeString(string(buffer)) 
-			message := block.Decrypt(ciphertextFromServer)  
-			fmt.Printf("## Receive From Server ##\n")
-			fmt.Printf("[Ciphertext] %x\n", ciphertextFromServer)
-			fmt.Printf("[message] %s\n\n", message)
-		}
-	}()
-
-	/*클라이언트가 서버에게 메세지를 보내는 부분*/
+	/*채팅 시작*/
+	go receive(conn)
+	go send(conn)
+	go handler(block)
 	for {
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n') //클라이언트 쪽에서 채팅을 입력
 		input = strings.TrimSpace(input)
 		input = "1" + input
-		ciphertext := block.Encrypt(input) 
-		sendMsg := base64.StdEncoding.EncodeToString(ciphertext) 
-
-		fmt.Printf("## Send to Server ##\n")
-		fmt.Printf("[message] %s\n", input[1:])
-		fmt.Printf("[Ciphertext] %x\n\n", ciphertext)
-		conn.Write([]byte(sendMsg)) 
+		inputChannel <- input
 	}
 }
 
-/* 대칭키 암호화 복호화 코드 */
+func handler(block *AESCipher) {
+	for {
+		select {
+		case buffer := <-receiveChannel: // receive 가 들어온 경우엔? 출력하기
+			ciphertextFromServer, _ := base64.StdEncoding.DecodeString(string(buffer)) //디코딩
+			message := block.Decrypt(ciphertextFromServer)                             //서버로부터 받은 암호문을 대칭키로 복호화
+			fmt.Printf("## Receive From Server ##\n")
+			fmt.Printf("[Ciphertext] %x\n", ciphertextFromServer)
+			fmt.Printf("[message] %s\n\n", message)
+		case input := <-inputChannel: // 입력값이 들어온 경우엔? send 하기
+			ciphertext := block.Encrypt(input)                       //채팅을 대칭키 암호화
+			sendMsg := base64.StdEncoding.EncodeToString(ciphertext) //인코딩
+			fmt.Printf("## Send to Server ##\n")
+			fmt.Printf("[message] %s\n", input[1:])
+			fmt.Printf("[Ciphertext] %x\n\n", ciphertext)
+			sendChannel <- []byte(sendMsg) //send channel 로 전송
+		}
+	}
+}
+
+func receive(conn net.Conn) {
+	for {
+		buffer := make([]byte, 2048)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			continue
+		}
+		receiveChannel <- buffer
+	}
+}
+
+func send(conn net.Conn) {
+	for {
+		message := <-sendChannel
+		conn.Write(message)
+	}
+}
+
+/*여기서부턴 구글링으로 찾은 대칭키 암호화, 복호화 코드를 약간 변형하였음*/
 type AESCipher struct {
 	block cipher.Block
 }
@@ -120,24 +144,32 @@ func NewAesCipher(key []byte) (*AESCipher, error) {
 	return &AESCipher{block}, nil
 }
 
-func (a *AESCipher) Encrypt(s string) []byte {
+func (a *AESCipher) Encrypt(s string) []byte { //대칭키 암호화 코드
 	byteString := []byte(s)
+
+	// Make the cipher text a byte array of size BlockSize + the length of the message
 	encryptByteArray := make([]byte, aes.BlockSize+len(s))
 
+	// The IV needs to be unique, but not secure. Therefore, it's common to include it at the beginning of the ciphertext.
 	iv := encryptByteArray[:aes.BlockSize]
-	io.ReadFull(rand.Reader, iv)
 
-	stream := cipher.NewCFBEncrypter(a.block, iv) 
+	io.ReadFull(rand.Reader, iv) //rand.Reader 에서 난수를 생성해서 iv에 넣기
+
+	// Encrypt the data
+	// iv는 블록의 블록 크기와 길이가 같아야 함
+	// 블록암호 : 평문의 동일 블록들이 하나의 메시지에서 동일한 암호문으로 되지 않도록 하기 위해 이전 암호 블록의 암호문을 다음 블록에 순서대로 적용
+	stream := cipher.NewCFBEncrypter(a.block, iv) //iv ==block size 여야 함
 	stream.XORKeyStream(encryptByteArray[aes.BlockSize:], byteString)
 
 	return encryptByteArray
 }
 
-func (a *AESCipher) Decrypt(byteString []byte) string {
+func (a *AESCipher) Decrypt(byteString []byte) string { //대칭키 복호화 코드
 
 	decryptByteArray := make([]byte, len(byteString))
-	iv := byteString[:aes.BlockSize]
+	iv := byteString[:aes.BlockSize] //첫번째 암호문에 대해서는 IV(Initial Vector)가 암호문 대신 사용
 
+	//Decrypt the data
 	stream := cipher.NewCFBDecrypter(a.block, iv)
 	stream.XORKeyStream(decryptByteArray, byteString[aes.BlockSize:])
 
--- server_file/server.go
+++ server_file/server.go
@@ -17,8 +17,12 @@ import (
 	"syscall"
 )
 
+var receiveChannel = make(chan []byte)
+var inputChannel = make(chan string)
+var sendChannel = make(chan []byte)
+
 func main() {
-	go func() {
+	go func() { //서버 종료 goroutine
 		signals := make(chan os.Signal, 1)
 		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 		<-signals
@@ -26,93 +30,110 @@ func main() {
 	}()
 
 	/* 서버 정보 */
-	serverPort := "12345"
+	serverPort := "12345" //서버의 포트번호
 
-	listener, _ := net.Listen("tcp", ":"+serverPort)
+	listener, _ := net.Listen("tcp", ":"+serverPort) //소켓 열기
 	fmt.Printf("## Server port number : %s ##\n\n", serverPort)
 
-	/* 공개키 암호화 알고리즘 - 개인키,공개키(비대칭키)를 생성 */
-	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
+	/* 공개키 암호화 알고리즘 - 개인키,공개키(비대칭키)를 생성 - 구글링으로 찾음 */
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512) //개인키와 공개키 생성
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	publicKey := &privateKey.PublicKey
+	publicKey := &privateKey.PublicKey //개인키 객체 안에 저장되어 있는 공개키를 따로 저장
 	fmt.Printf("## RSA private key and public key is generated ##\n\n")
 
+	/* 클라이언트 연결 */
+	conn, _ := listener.Accept() //클라이언트로부터 연결 요청 듣기
+
+	/*클라이언트에 공개키를 보내는 부분*/
+	pubInJason, err := json.Marshal(publicKey)
+	pub2 := &rsa.PublicKey{}
+	err = json.Unmarshal(pubInJason, pub2)                   //공개키를 소켓통신을 통해 보낼 수 있도록 형식을 다듬어줌
+	sendMsg := base64.StdEncoding.EncodeToString(pubInJason) //인코딩
+	fmt.Printf("## Send public key to Client %s ##\n", conn.RemoteAddr().String())
+	fmt.Printf("[Public key] %x\n\n", publicKey)
+	conn.Write([]byte(sendMsg)) //클라이언트에게 공개키 보냄
+
+	/*클라이언트에서 응답으로 대칭키가 오는 부분*/
+	buffer := make([]byte, 2048)
+	conn.Read(buffer) //대칭키 읽음
+	ciphertext, _ := base64.StdEncoding.DecodeString(string(buffer))
+	/*암호화된 대칭키를 개인 키로 복호화 - 개인키 복호화 코드는 구글링으로 찾음*/
+	plaintext, err := rsa.DecryptPKCS1v15( //복호화해서 대칭키 추출
+		rand.Reader,
+		privateKey, // 개인키
+		ciphertext,
+	)
+	symmetricKey := string(plaintext) //대칭키를 string 형식으로 변환
+	fmt.Printf("## Receive symmetric key from Client %s ##\n", conn.RemoteAddr().String())
+	fmt.Printf("[Ciphertext] %x\n", ciphertext)
+	fmt.Printf("[symmetric key] %s\n\n", symmetricKey)
+	//응답 끝
+
+	/* 대칭키 암호화에 사용할 대칭키 블록 생성 */
+	block, _ := NewAesCipher([]byte(symmetricKey))
+
+	/*채팅 시작*/
+	go receive(conn)
+	go send(conn)
+	go handler(conn, block)
+
 	for {
-		/* 클라이언트 연결 */
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+		inputChannel <- input
+	}
+}
 
-		/*클라이언트에 공개키를 보내는 부분*/
-		pubInJason, err := json.Marshal(publicKey)
-		pub2 := &rsa.PublicKey{}
-		err = json.Unmarshal(pubInJason, pub2)      
-		sendMsg := base64.StdEncoding.EncodeToString(pubInJason) 
-		fmt.Printf("## Send public key to Client %s ##\n", conn.RemoteAddr().String())
-		fmt.Printf("[Public key] %x\n\n", publicKey)
-		conn.Write([]byte(sendMsg)) 
-
-		/*클라이언트에서 응답으로 대칭키가 오는 부분*/
-		buffer := make([]byte, 2048)
-		conn.Read(buffer) 
-		ciphertext, _ := base64.StdEncoding.DecodeString(string(buffer))
-		/*암호화된 대칭키를 개인 키로 복호화 */
-		plaintext, err := rsa.DecryptPKCS1v15( 
-			rand.Reader,
-			privateKey, 
-			ciphertext,
-		)
-		symmetricKey := string(plaintext)
-		fmt.Printf("## Receive symmetric key from Client %s ##\n", conn.RemoteAddr().String())
-		fmt.Printf("[Ciphertext] %x\n", ciphertext)
-		fmt.Printf("[symmetric key] %s\n\n", symmetricKey)
-
-		/* 대칭키 암호화에 사용할 대칭키 블록 생성 */
-		block, _ := NewAesCipher([]byte(symmetricKey))
-
-		/* 메세지를 받는 부분 */
-		go func() {
-			for {
-				buffer := make([]byte, 2048)
-				_, err := conn.Read(buffer)
-				if err != nil {
-					continue
-				}
-				fmt.Printf("## Receive from Client %s ##\n", conn.RemoteAddr().String())
-				ciphertextFromClient, _ := base64.StdEncoding.DecodeString(string(buffer))
-				message := block.Decrypt(ciphertextFromClient)
-				option := message[:1]
-				if option == "1" { //메세지의 맨 앞의 옵션이 1이면 채팅이고 아니면 종료하도록 함
-					fmt.Printf("[Ciphertext] %x\n", ciphertextFromClient)
-					fmt.Printf("[message] %s\n\n", message[1:])
-				} else { //종료
-					fmt.Printf("## Client %s disconnected ##\n\n", conn.RemoteAddr().String())
-					conn.Close()
-					break
-				}
+func handler(conn net.Conn, block *AESCipher) {
+	for {
+		select {
+		case buffer := <-receiveChannel: // receive 가 들어온 경우엔? 출력하기
+			fmt.Printf("## Receive from Client %s ##\n", conn.RemoteAddr().String())
+			ciphertextFromClient, _ := base64.StdEncoding.DecodeString(string(buffer))
+			message := block.Decrypt(ciphertextFromClient)
+			option := message[:1]
+			if option == "1" { //메세지의 맨 앞의 옵션이 1이면 채팅이고 아니면 종료하도록 함
+				fmt.Printf("[Ciphertext] %x\n", ciphertextFromClient)
+				fmt.Printf("[message] %s\n\n", message[1:])
+			} else { //종료
+				fmt.Printf("## Client %s disconnected ##\n\n", conn.RemoteAddr().String())
+				conn.Close()
+				os.Exit(0)
+				return
 			}
-		}()
-
-		/* 메세지를 보내는 부분*/
-		for {
-			input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-			input = strings.TrimSpace(input)
-			ciphertext := block.Encrypt(input)
-			sendMsg := base64.StdEncoding.EncodeToString(ciphertext)
-
-			conn.Write([]byte(sendMsg)) //송신
+		case input := <-inputChannel: // 입력값이 들어온 경우엔? send 하기
+			ciphertext := block.Encrypt(input)                       //채팅을 대칭키 암호화
+			sendMsg := base64.StdEncoding.EncodeToString(ciphertext) //인코딩
 			fmt.Printf("## Send to Client %s ##\n", conn.RemoteAddr().String())
 			fmt.Printf("[message] %s\n", input)
 			fmt.Printf("[Ciphertext] %x\n\n", ciphertext)
+			sendChannel <- []byte(sendMsg) //send 채널로 전송
 		}
 	}
 }
 
-/* 대칭키 암호화, 복호화 코드 */
+func receive(conn net.Conn) {
+	for {
+		buffer := make([]byte, 2048)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			continue
+		}
+		receiveChannel <- buffer
+	}
+}
+
+func send(conn net.Conn) {
+	for {
+		message := <-sendChannel
+		conn.Write(message)
+	}
+}
+
+/*여기서부턴 구글링으로 찾은 대칭키 암호화, 복호화 코드를 약간 변형하였음*/
 type AESCipher struct {
 	block cipher.Block
 }
@@ -125,11 +146,12 @@ func NewAesCipher(key []byte) (*AESCipher, error) {
 	return &AESCipher{block}, nil
 }
 
-func (a *AESCipher) Encrypt(s string) []byte {
+func (a *AESCipher) Encrypt(s string) []byte { //대칭키 암호화 코드
 	byteString := []byte(s)
 	encryptByteArray := make([]byte, aes.BlockSize+len(s))
 
 	iv := encryptByteArray[:aes.BlockSize]
+
 	io.ReadFull(rand.Reader, iv)
 
 	stream := cipher.NewCFBEncrypter(a.block, iv)
@@ -138,7 +160,7 @@ func (a *AESCipher) Encrypt(s string) []byte {
 	return encryptByteArray
 }
 
-func (a *AESCipher) Decrypt(byteString []byte) string {
+func (a *AESCipher) Decrypt(byteString []byte) string { //대칭키 복호화 코드
 
 	decryptByteArray := make([]byte, len(byteString))
 	iv := byteString[:aes.BlockSize]
